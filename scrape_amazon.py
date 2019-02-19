6d9701dde4e4ab431358fbe71d20468c415fa4f5
Knwright <43178849+knwright@users.noreply.github.com>
2019-02-19 17:50:54 -0600

    Update scrape_amazon.py

--- scrape_amazon.py
+++ scrape_amazon.py
@@ -1,7 +1,22 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
+import csv
+from urllib3.request import urlopen
 
+url_list = []
+# Open the url file in read mode
+with open("urlfile.csv", "r") as csvf:
+    urls = csv.reader(csvf)
+    for url in urls:
+        # Add each url to the url_list
+        url_list.append(url)
+
+for url in url_list:
+    page = urlopen(url[0]).read()
+    soup = BeautifulSoup(page, "html.parser")
+
+print(soup)
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
@@ -12,38 +27,7 @@ def init_browser():
 def scrape_info():
     browser = init_browser()
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
-    browser.visit(url)
 
-    time.sleep(1)
-
-    # Scrape page into Soup
-    html = browser.html
-    soup = bs(html, "html.parser")
-
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
-
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
-
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
-
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
-
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
-
-    # Close the browser after scraping
-    browser.quit()
 
     # Return results
     return costa_data
